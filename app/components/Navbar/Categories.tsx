5fae8f7588ac6730cd26241bce500439e39c7a16
Ranjan yadav <theranzanydv@gmail.com>
2024-04-21 00:29:29 +0545

    Update icons and descriptions in Navbar Categories component

--- app/components/Navbar/Categories.tsx
+++ app/components/Navbar/Categories.tsx
@@ -2,40 +2,49 @@
 
 import { usePathname, useSearchParams } from 'next/navigation'
 import {
+	GiGrass,
+	GiRoad,
 	GiWindmill
 } from 'react-icons/gi'
-import { MdOutlineVilla } from 'react-icons/md'
+import { MdOutlineVilla, MdRoofing } from 'react-icons/md'
 import { TbBeach, TbMountain } from 'react-icons/tb'
 import CategoryBox from '../CategoryBox'
 import { Container } from '../Container'
+import { BiBed, BiDiamond } from 'react-icons/bi'
+import { IoBed } from 'react-icons/io5'
 
 
 
 export const categories = [
 	{
 		label: 'Deluxe',
-		icon: TbBeach,
-		description: 'This Property is close to the beach',
+		icon: BiDiamond,
+		description: 'This is a deluxe room. It has all the luxury amenities',
 	},
 	{
 		label: 'Single Bed',
-		icon: GiWindmill,
-		description: 'This Property has windmills',
+		icon: BiBed,
+		description: 'This room has a single bed. Perfect for solo travelers',
 	},
 	{
 		label: 'Double Bed',
-		icon: MdOutlineVilla,
-		description: 'This Property is modern',
+		icon: IoBed,
+		description: 'This room has a double bed. Perfect for couples',
 	},
 	{
 		label: 'Road Facing',
-		icon: TbMountain,
-		description: 'This Property in the countryside',
+		icon: GiRoad,
+		description: 'This room is road facing, perfect for people who love the city life',
 	},
 	{
 		label: 'Garden Facing',
-		icon: TbMountain,
-		description: 'This Property in the countryside',
+		icon: GiGrass,
+		description: 'This room is garden facing, perfect for people who love nature',
+	},
+	{
+		label: 'Roof Top',
+		icon: MdRoofing,
+		description: 'This room is on the roof top, perfect for people who love the view',
 	},
 
 ]
